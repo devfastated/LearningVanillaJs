5b0d423a1f5d08f0ed887998ad5df72f7069a0fd
Romer Jasen Jimenez <businessmailromer@gmail.com>
2023-10-31 14:15:08 +0800

    second commit

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// this is single comment in Javascipt
+/*
+    This is multiple comment in Javasctip
+ */
+
+    // alert("This is alert in Javascript");
+    // const name = "Romer Jasen Jimenez";
+    // console.log(name);
+    // document.getElementById("app").innerHTML = ("this code gets the id element in html and output it");
+
+
+
+    alert("Some strict");
