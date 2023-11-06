00098f006763577e800a785c46cd5e9394a687c1
Romer Jimenez <116239847+Reraizen@users.noreply.github.com>
2023-11-06 22:30:52 +0800

    javascriptishell

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert( 1n + 12n);
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+      console.log(`Sub-array ${i}: ${arr[i]}`);
+      for (let j = 0; j < arr[i].length; j++) {
+        product *= arr[i][j];
+      }
+    }
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1,2],[3,4],[5,6,7]]);
